e8a2f762a5167ca70035a0e2f095f65d640a6804
guillaumefontan <gfont48@gmail.com>
2025-02-24 17:31:20 +0100

    pre commit

--- examples/sdk_test.py
+++ examples/sdk_test.py
@@ -3,7 +3,7 @@ import os
 
 from trubrics import Trubrics
 
-trubrics = Trubrics(api_key=os.environ["TRUBRICS_API_KEY"], host="http://127.0.0.1:8001/api/ingestion")
+trubrics = Trubrics(api_key=os.environ["TRUBRICS_API_KEY"])
 trubrics.logger.setLevel(logging.DEBUG)
 
 
@@ -11,7 +11,7 @@ trubrics.track(event="Page view", user_id="user_id", properties={"page": "home"}
 trubrics.track(event="Page view", user_id="user_id", properties={"page": "events"})
 
 trubrics.track_llm(
-    user_id="user_id",
+    user_id="sdk_test",
     prompt="What is Trubrics?",
     assistant_id="gpt4o",
     generation="Trubrics is a product analytics platform for AI applications.",
